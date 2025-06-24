d42e917487ad425aac0db15d0695d3927502d82b
Kiran Gadamsetty <gadamsettykiran514@gmail.com>
2025-06-24 18:51:06 +0530

    add changes

--- src/components/Shimmer.jsx
+++ src/components/Shimmer.jsx
@@ -3,11 +3,11 @@ function Shimmer(){
     return(
 <div style = {{marginTop:"150px"}}>
 <Container>
-    <Row className = "d-flex justify-content-evenly">
+    <Row className = "d-flex justify-content-evenly d-none d-lg-block">
        
-        {[1,2, 3, 4,5].map(()=>{
+        {[1,2, 3, 4,5].map((key)=>{
             return(
-                 <Col xs = {2}>
+                 <Col key  = {key} xs = {2}>
 <p className = "bg-body-secondary" style = {{height:"200px",width:"200px",borderRadius:"100%", backgroundColor:"lightblue"}}></p>
                  </Col>
             )
@@ -16,13 +16,13 @@ function Shimmer(){
        
     </Row>
     <Row className   ="mt-4">
-    <Col xs= {12}>
+    <Col xs= {12} >
         <h1 style = {{width:"300px", padding:"10px",borderRadius:"5px"}} className = "bg-body-secondary my-2"></h1>
      <h1 style = {{width:"200px", padding:"10px",borderRadius:"5px"}} className = "bg-body-secondary mt-2"></h1>
     </Col>
-        {[1,2,3,4,5,6,7,8].map(()=>{
+        {[1,2,3,4,5,6,7,8].map((key)=>{
             return(
-                <Col sm={3}>
+                <Col key = {key} xs={6} sm={6} md={4} lg={4} xl={3} >
                     <p className = "bg-body-secondary" style = {{height:"300px",gap:"15px", backgroundColor:"lightblue", borderRadius:"5px"}}></p>
                 </Col>
             )
